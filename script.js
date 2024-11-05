8814a038b644627a530ab577fe5efbdc1cbf7605
Ritu raj <79391519+RRAJ644@users.noreply.github.com>
2024-11-06 00:02:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function displayFormInfo() {
+
+	const form = document.getElementById('form')
+	
+	const firstName = form.elements['First Name'].value
+	const lastName = form.elements['Last Name'].value
+	const phoneNumber = form.elements['Phone Number'].value
+	const emailId = form.elements['Email Id'].value
+		
+	const message = `First Name: ${firstName} Last Name: ${lastName} Phone Number: ${phoneNumber} Email ID: ${emailId}`;
+
+	alert(message)
+}
